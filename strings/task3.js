b73bfef9890d3ef1bf28f3290348c5c168c08e75
NikGeht <main@nikgeht.ru>
2025-05-01 15:50:50 +0700

    done task4 strings

--- strings/task3.js
+++ strings/task3.js
@@ -1,3 +1,6 @@
 const userName = prompt('Как вас зовут?')
+let userAge = prompt('Сколько вам лет?')
+userAge = Number(userAge.trim())
 
-alert(`Вас зовут ${userName.toUpperCase().trim()}`)
+
+alert(`Вас зовут ${userName.toUpperCase().trim()} и вам ${userAge} лет`)
