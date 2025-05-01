982a34e1cf27aa7828e5d76bd4b50781f2117ea9
NikGeht <main@nikgeht.ru>
2025-05-01 15:32:36 +0700

    done task1 strings

---
+++ strings/index.js
@@ -0,0 +1,7 @@
+const myName = 'Никита'
+const programmingLanguage = 'JavaScript'
+const courseCreatorName = 'Владилен Минин'
+const numberOfMonth = '2'
+const reasonText = 'захотелось переквалифицировать в фронтенд-разработчика'
+console.log(`Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев(а). Я уверен(а), что пройду данный курс до конца!`)
+
