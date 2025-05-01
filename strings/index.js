eb408d430bc0814d12685f8aec887543b4e96b90
NikGeht <main@nikgeht.ru>
2025-05-01 15:39:07 +0700

    done task2 strings

--- strings/index.js
+++ strings/index.js
@@ -5,3 +5,10 @@ const numberOfMonth = '2'
 const reasonText = 'захотелось переквалифицировать в фронтенд-разработчика'
 console.log(`Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев(а). Я уверен(а), что пройду данный курс до конца!`)
 
+let myInfoText = `Всем привет! Меня зовут ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на курсе по ${programmingLanguage} у ${courseCreatorName}. Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев(а). Я уверен(а), что пройду данный курс до конца!`
+
+let myInfoTextLowerJS = myInfoText.replaceAll('JavaScript', 'javascript')
+let myInfoTextLowerJSUpperCourse = myInfoTextLowerJS.replaceAll('курс', 'КУРС')
+
+myInfoText = myInfoTextLowerJSUpperCourse
+console.log(`${myInfoText} Длина - ${myInfoText.length}, Первый символ - ${myInfoText[0]}, Последний символ - ${myInfoText[myInfoText.length - 1]}`)
