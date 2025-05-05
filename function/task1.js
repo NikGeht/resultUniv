7c995ead71edfa8caaf5dca3e32f4d8e8824a5ef
NikGeht <main@nikgeht.ru>
2025-05-05 20:49:29 +0700

    done task1 function

---
+++ function/task1.js
@@ -0,0 +1,19 @@
+function getName1(name){
+    return `Имя равно ${name}`
+}
+
+let getName2 = function(name){
+    return `Имя равно ${name}`
+}
+
+let getName3 = (name) => {
+    return `Имя равно ${name}`
+}
+
+console.log(getName1('Peter'))
+console.log(getName2('Vasya'))
+console.log(getName3('Nikita'))
+
+
+
+
