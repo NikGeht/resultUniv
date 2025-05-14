c468b7ebcbe62a6751e401860a0e8982481b05fe
NikGeht <main@nikgeht.ru>
2025-05-14 17:42:35 +0700

    done dopTask5

--- date/dopTask/index.js
+++ date/dopTask/index.js
@@ -206,5 +206,91 @@ const resultWinner = getWinner(winnerApplicants, todaysWinner);
 console.log('resultWinner', resultWinner); 
 // { prize: '10 000$', name: 'Максим', age: 25 }
 
+// Task 5
+
+console.log(`dopTask5`)
+
+const usersArray = [
+    { id: '34rdca3eeb7f6fgeed471198', name: 'Andrew', age: 25 },
+    { id: '76rdca3eeb7f6fgeed471100', name: 'Alexey', age: 15 },
+    { id: '12rdca3eeb7f6fgeed4711012', name: 'Egor', age: 13 },
+    { id: '32rdca3eeb7f6fgeed471101', name: 'Kate', age: 31 },
+    { id: '98rdca3eeb7f6fgeed471102', name: 'Elena', age: 18 }
+];
+  
+const usersObject = {
+    '34rdca3eeb7f6fgeed471198': { 
+    id: '34rdca3eeb7f6fgeed471198', 
+    name: 'Andrew', 
+    age: 25 
+    },
+    '76rdca3eeb7f6fgeed471100': { 
+    id: '76rdca3eeb7f6fgeed471100', 
+    name: 'Alexey', 
+    age: 15 
+    }, 
+    '12rdca3eeb7f6fgeed4711012': { 
+    id: '12rdca3eeb7f6fgeed4711012', 
+    name: 'Egor', 
+    age: 13 
+    },
+    '32rdca3eeb7f6fgeed471101': { 
+    id: '32rdca3eeb7f6fgeed471101', 
+    name: 'Kate', 
+    age: 31 
+    },
+    '98rdca3eeb7f6fgeed471102': { 
+    id: '98rdca3eeb7f6fgeed471102', 
+    name: 'Elena', 
+    age: 18 
+    }
+};
+
+function getAdultUsers(users) {
+    if (Array.isArray(users)) {
+        return users.filter(person => person.age >= 18)
+    } else {
+        const keyUsers = Object.keys(users).filter(person => users[person].age >= 18)
+        const adultUsers = {}
+        for (let keyUser of keyUsers) {
+            adultUsers[keyUser] = users[keyUser]
+        }
+        return adultUsers
+        
+    }
+    
+    
+}
+  
+  console.log(getAdultUsers(usersArray));
+/*
+    [
+        { id: '34rdca3eeb7f6fgeed471198', name: 'Andrew', age: 25 },
+        { id: '32rdca3eeb7f6fgeed471101', name: 'Kate', age: 31 },
+        { id: '98rdca3eeb7f6fgeed471102', name: 'Elena', age: 18 }
+    ]
+*/
+console.log(usersObject)
+console.log(getAdultUsers(usersObject));
+/*
+    {
+        '34rdca3eeb7f6fgeed471198': { 
+        id: '34rdca3eeb7f6fgeed471198', 
+        name: 'Andrew', 
+        age: 25 
+    },
+        '32rdca3eeb7f6fgeed471101': { 
+        id: '32rdca3eeb7f6fgeed471101', 
+        name: 'Kate', 
+        age: 31 
+    },
+        '98rdca3eeb7f6fgeed471102': { 
+        id: '98rdca3eeb7f6fgeed471102', 
+        name: 'Elena', 
+        age: 18 
+    }
+}
+*/
+
 
 
