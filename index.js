ced23f878db22abb77723013ea156c3d449e22c2
NikGeht <main@nikgeht.ru>
2025-06-10 17:27:29 +0700

    add new ERROR for async functions

--- index.js
+++ index.js
@@ -2,46 +2,49 @@ const POST_URL = `https://jsonplaceholder.typicode.com/posts/`;
 const COMMENTS_URL = `https://jsonplaceholder.typicode.com/comments?postId=`;
 
 function createPostElement(title, text, comments) {
-    const postElement = document.createElement('div');
-    postElement.id = 'post';
-    postElement.className = 'post';
+    const postElement = document.createElement("div");
+    postElement.id = "post";
+    postElement.className = "post";
 
-    const titlePost= document.createElement('h1');
-    titlePost.className = 'post__title'
+    const titlePost = document.createElement("h1");
+    titlePost.className = "post__title";
     titlePost.textContent = title;
-    const textPost = document.createElement('p');
-    textPost.className = 'post__text';
+    const textPost = document.createElement("p");
+    textPost.className = "post__text";
     textPost.textContent = text;
 
-    const headerComment = document.createElement('b');
-    headerComment.className = 'post__comments-text';
-    headerComment.textContent = 'Комментарии';
+    const headerComment = document.createElement("b");
+    headerComment.className = "post__comments-text";
+    headerComment.textContent = "Комментарии";
 
     const commentsContainer = createCommentsElement(comments);
+    if (!commentsContainer) {
+        return null;
+    }
 
-    postElement.append(titlePost, textPost, headerComment);
-    postElement.append(commentsContainer);
+    postElement.append(titlePost, textPost, headerComment, commentsContainer);
 
-    document.body.append(postElement);
-    
+    return postElement;
 }
 
 function createCommentsElement(comments) {
-    const commentsContainer = document.createElement('div');
-    commentsContainer.className = 'post__comments';
+    if (!Array.isArray(comments)) {
+        return null;
+    }
+    const commentsContainer = document.createElement("div");
+    commentsContainer.className = "post__comments";
 
     for (let comment of comments) {
-        console.log(comment);
-        const commentContainer = document.createElement('div');
-        commentContainer.className = 'post-comment';
+        const commentContainer = document.createElement("div");
+        commentContainer.className = "post-comment";
 
-        const authorComment = document.createElement('span');
-        authorComment.className = 'post-comment__author';
-        authorComment.textContent = comment?.email;
+        const authorComment = document.createElement("span");
+        authorComment.className = "post-comment__author";
+        authorComment.textContent = comment?.email || "Empty email";
 
-        const textComment = document.createElement('span');
-        textComment.className = 'post-comment__text';
-        textComment.textContent = comment?.body;
+        const textComment = document.createElement("span");
+        textComment.className = "post-comment__text";
+        textComment.textContent = comment?.body || "Empty body";
 
         commentContainer.append(authorComment, textComment);
         commentsContainer.append(commentContainer);
@@ -52,41 +55,55 @@ function createCommentsElement(comments) {
 async function fetchingUrl(url) {
     try {
         const request = await fetch(url, {
-            method: 'GET',
-        })
+            method: "GET",
+        });
         const response = await request.json();
-        
+
         return response;
     } catch (error) {
         console.error(error);
+        return null;
     }
-    
 }
 
 async function getPostWithComments(postId) {
     try {
         const post = await fetchingUrl(`${POST_URL}${postId}`);
+        if (!post) {
+            throw new Error("Пост не найден");
+        }
 
         const comments = await fetchingUrl(`${COMMENTS_URL}${postId}`);
-                
-        return {post, comments};
+        if (!comments) {
+            throw new Error("Комментарии не найдены");
+        }
+        return { post, comments };
     } catch (error) {
         console.error(error);
+        return null;
     }
-
-    
 }
 
 async function renderPost(postId) {
     try {
-        const {post, comments} = await getPostWithComments(postId);
-        console.log('IN RENDER POST', post, comments);
+        const { post, comments } = await getPostWithComments(postId);
 
-        createPostElement(post.title, post.body, comments);
+        if (!post || !comments) {
+            throw new Error("Не удалось получить пост или комментарии");
+        }
+        const { title, body } = post;
+        if (!title || !body) {
+            throw new Error("Не удалось получить данные поста");
+        }
+        const postElement = createPostElement(title, body, comments);
+        if (!postElement) {
+            throw new Error("Не удалось создать элемент поста");
+        }
+        document.body.append(postElement);
     } catch (error) {
         console.error(error);
+        return null;
     }
-    
 }
 
 renderPost(1);
