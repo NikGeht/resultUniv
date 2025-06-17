9410cec959d788bb31c094d68ba315d5810b4c72
NikGeht <main@nikgeht.ru>
2025-06-17 19:04:40 +0700

    done task4 webpack

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import './index.css';
+import jsLogo from './assets/javascript.png';
 console.log('Hello World!');
+
+const body = document.body;
+const textJS = document.createElement('h1');
+textJS.textContent = 'I love JavaScript';
+
+const imgJS = document.createElement('img');
+imgJS.src = jsLogo;
+
+body.append(textJS, imgJS);
--- webpack.config.js
+++ webpack.config.js
@@ -10,23 +10,37 @@ module.exports = {
       path: path.resolve(__dirname, 'dist'),
       clean: true
     },
-    // plugins: [
-    //     new HtmlWebpackPlugin({
-    //         template: path.resolve(__dirname, 'index.html')
-    //     })
-    // ],
-    // module: {
-    //     rules: [
-    //       {
-    //         test: /.m?js$/,
-    //         exclude: /(node_modules|bower_components)/,
-    //         use: {
-    //           loader: 'babel-loader',
-    //           options: {
-    //             presets: ['@babel/preset-env']
-    //           }
-    //         }
-    //       }
-    //     ]
-    //   }
+    devServer: {
+        static: path.resolve(__dirname, 'dist'),
+        port: 8080,
+        open: true,
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'index.html')
+        })
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+        ],
+        
+      },
 };
