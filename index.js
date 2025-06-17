7b0320605237cc1acb821cad2463547eb366d756
NikGeht <main@nikgeht.ru>
2025-06-17 18:19:28 +0700

    done task2 bundler

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import './index.css';
 console.log('Hello World!');
+
+const body = document.body;
+const textJS = document.createElement('h1');
+textJS.textContent = 'I love JavaScript';
+
+const imgJS = document.createElement('img');
+imgJS.src = 'assets/javascript.png';
+
+body.append(textJS, imgJS);
