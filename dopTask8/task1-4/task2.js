2c3318478b1b778c01b27a1b55c3510553977bce
NikGeht <main@nikgeht.ru>
2025-06-24 12:34:54 +0700

    another fix

--- dopTask8/task1-4/task2.js
+++ dopTask8/task1-4/task2.js
@@ -4,10 +4,7 @@ console.log(isEqualSymbols('ноутбук', 'программист')) // false
 function isEqualSymbols(str1, str2) { 
     const setStr1 = new Set(str1);
     const setStr2 = new Set(str2);
-    console.log(setStr1, setStr2);
-    if (setStr1.size !== setStr2.size) {
-        return false;
-    } else {
-        return setStr1.has(...setStr2);
-    }
+    
+    if (setStr1.size !== setStr2.size) return false;
+    return Array.from(setStr1).every(char => setStr2.has(char));
 }
