e400cc8999336f9dba831dc4defc3435ed3d3a26
NikGeht <main@nikgeht.ru>
2025-06-24 09:37:37 +0700

    add app.js

--- dopTask8/task5/index.js
+++ dopTask8/task5/index.js
@@ -1,32 +1,5 @@
 import "./index.css";
-import { initState, setState } from "./src/state";
-import {
-    checkButtonInLocalStorage,
-    hideCookieConsent,
-    showCookieConsent,
-} from "./src/cookies";
-import { BUTTON_CLASS } from "./src/constants";
 
-/**
- * @description Запускает приложение
- * @returns {void}
- */
-function runApp() {
-    initState();
-
-    if (checkButtonInLocalStorage()) {
-        hideCookieConsent();
-    } else {
-        showCookieConsent();
-    }
-
-    const button = document.querySelector(`.${BUTTON_CLASS}`);
-    if (button) {
-        button.addEventListener("click", () => {
-            setState("true");
-            hideCookieConsent();
-        });
-    }
-}
+import { runApp } from "./src/app";
 
 runApp();
---
+++ dopTask8/task5/src/app.js
@@ -0,0 +1,25 @@
+import { initState, setState } from "./state";
+import { checkButtonInLocalStorage, hideCookieConsent, showCookieConsent } from "./cookies";
+import { BUTTON_CLASS } from "./constants";
+
+/**
+ * @description Запускает приложение
+ * @returns {void}
+ */
+export function runApp() {
+    initState();
+
+    if (checkButtonInLocalStorage()) {
+        hideCookieConsent();
+    } else {
+        showCookieConsent();
+    }
+
+    const button = document.querySelector(`.${BUTTON_CLASS}`);
+    if (button) {
+        button.addEventListener("click", () => {
+            setState("true");
+            hideCookieConsent();
+        });
+    }
+}
