da37a01e96601ffae2464aa7a0db7a784262a0bc
NikGeht <main@nikgeht.ru>
2025-07-01 11:59:50 +0700

    react: done task1 with react.createElement

--- react-task1/src/App.jsx
+++ react-task1/src/App.jsx
@@ -5,27 +5,32 @@ import './App.css';
 export function App() {
 	const [count, setCount] = useState(0);
 
-	return (
-		<div>
-			<div>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<p>{new Date().getFullYear()}</p>
-		</div>
+	return React.createElement('div', null,
+		React.createElement('div', null,
+			React.createElement('a', {
+				href: 'https://react.dev',
+				target: '_blank'
+			},
+				React.createElement('img', {
+					src: reactLogo,
+					className: 'logo react',
+					alt: 'React logo'
+				})
+			)
+		),
+		React.createElement('h1', null, 'Vite + React'),
+		React.createElement('div', {
+			className: 'card'
+		},
+			React.createElement('button', {
+				onClick: () => setCount((count) => count + 1)
+			}, 'count is ' + count),
+			React.createElement('p', null, 'Edit code and save to test HMR'),
+			React.createElement('p', {
+				className: 'read-the-docs'
+			}, 'Click on the Vite and React logos to learn more'),
+			React.createElement('p', null, new Date().getFullYear())
+		)
 	);
 }
 
