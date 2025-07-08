347487bd6f6347c57b9d7fa3043a8c385e174a9c
NikGeht <main@nikgeht.ru>
2025-07-08 09:48:31 +0700

    add for active step class done

--- react/comp-task2/src/components/StepsList/StepsList.jsx
+++ react/comp-task2/src/components/StepsList/StepsList.jsx
@@ -3,31 +3,29 @@ import styles from './stepslist.module.css';
 export const StepsList = ({ steps, activeIndex, handlerButtonStepClick }) => {
 	return (
 		<ul className={styles['steps-list']}>
-						{steps.map((step) => {
-							return (
-								<li
-									key={step.id}
-									className={
-										styles['steps-item'] +
-										' ' +
-										(activeIndex === Number(step.id)
-											? styles.active
-											: '') +
-										' ' +
-										(activeIndex > Number(step.id) ? styles.done : '')
-									}
-								>
-									<button
-										key={step.id}
-										className={styles['steps-item-button']}
-										onClick={handlerButtonStepClick}
-									>
-										{Number(step.id)}
-									</button>
-									Шаг {Number(step.id)}
-								</li>
-							);
-						})}
-					</ul>
+			{steps.map((step) => {
+				return (
+					<li
+						key={step.id}
+						className={
+							styles['steps-item'] +
+							' ' +
+							(activeIndex === Number(step.id) ? styles.active : '') +
+							' ' +
+							(activeIndex >= Number(step.id) ? styles.done : '')
+						}
+					>
+						<button
+							key={step.id}
+							className={styles['steps-item-button']}
+							onClick={handlerButtonStepClick}
+						>
+							{Number(step.id)}
+						</button>
+						Шаг {Number(step.id)}
+					</li>
+				);
+			})}
+		</ul>
 	);
-}
+};
